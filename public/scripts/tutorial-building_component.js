04d20bab167aa55fd8e28aa4a142d26cc793752c
hikariru <ranai@Hikariru.local>
2016-05-22 03:38:27 +0900

    created components
    * データが子要素(?)に向かって明示的に送りつけられてるのがAngularJSと違ってお行儀よさそう

--- public/scripts/tutorial-building_component.js
+++ public/scripts/tutorial-building_component.js
@@ -1,11 +1,20 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment."},
+  {author: "Jordan Walke", text: "This is *another* comment."}
+];
+
 var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString());
+    return { __html: rawMarkup }
+  },
   render: function () {
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
@@ -13,10 +22,17 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment.</Comment>
-        <Comment author="Jordan Walke">This is *another* comment.</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -37,7 +53,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -45,6 +61,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render(
-  React.createElement(CommentBox, null),
+  React.createElement(CommentBox, data={data}),
   document.getElementById('content')
 );
